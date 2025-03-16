658b8dcb6cfe36ad5f06c53f6e48056512c0e3d9
gram <git@orsinium.dev>
2025-03-16 09:35:57 +0100

    show "connecting" message

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import "github.com/firefly-zero/firefly-go/firefly"
+
+var (
+	peers firefly.Peers
+	font  firefly.Font
+	frame uint32
+	me    firefly.Peer
+)
+
+const (
+	FontHeight = 10
+	FontWidth  = 6
+	X          = 120 - 3*13
+	Y          = 71
+)
+
+func init() {
+	firefly.Boot = boot
+	firefly.Update = update
+	firefly.Render = render
+}
+
+func boot() {
+	font = firefly.LoadFile("font", nil).Font()
+}
+
+func update() {
+	frame += 1
+	me = firefly.GetMe()
+	newPeers := firefly.GetPeers()
+	if newPeers != peers {
+		peers = newPeers
+	}
+}
+
+func render() {
+	firefly.ClearScreen(firefly.ColorWhite)
+
+	point := firefly.Point{X: X, Y: Y - FontHeight}
+	text := "Connecting..."
+	firefly.DrawText(text, font, point, firefly.ColorGray)
+
+	step := int(frame) / 5
+	var shift int
+	length := len(text) + 1
+	if step%(length*2) >= length {
+		shift = step % length
+		text = text[step%length:]
+	} else {
+		shift = 0
+		text = text[:step%length]
+	}
+	point = firefly.Point{X: X + shift*FontWidth, Y: Y - FontHeight}
+	firefly.DrawText(text, font, point, firefly.ColorBlack)
+
+	// for i, peer := range peers.Slice() {
+	// 	name := firefly.GetName(peer)
+	// 	if name == "" {
+	// 		name = "???"
+	// 	}
+	// }
+}
