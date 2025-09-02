394601b9bafc5df536e6949f09c6ae7bc00c24c0
gram <git@orsinium.dev>
2025-09-02 17:11:06 +0200

    neobrutalist background, update SDK

--- main.go
+++ main.go
@@ -115,15 +115,57 @@ func render() {
 	if exited {
 		return
 	}
-	firefly.ClearScreen(firefly.ColorWhite)
+	drawBackgroundGrid()
+	drawBackgroundBox()
 	if !stopped {
-		drawConnecting()
+		drawLoadingBar()
 	}
 	drawPeers()
 	drawButtons()
 }
 
-func drawConnecting() {
+func drawBackgroundGrid() {
+	const cellSize = 10
+	firefly.ClearScreen(firefly.ColorWhite)
+	lineStyle := firefly.L(firefly.ColorLightGray, 1)
+	for x := cellSize; x < firefly.Width; x += cellSize {
+		firefly.DrawLine(
+			firefly.P(x, 0),
+			firefly.P(x, firefly.Height),
+			lineStyle,
+		)
+	}
+	for y := cellSize; y < firefly.Height; y += cellSize {
+		firefly.DrawLine(
+			firefly.P(0, y),
+			firefly.P(firefly.Width, y),
+			lineStyle,
+		)
+	}
+}
+
+func drawBackgroundBox() {
+	const margin = 15
+	size := firefly.S(firefly.Width-margin*2, firefly.Height-margin*2)
+	firefly.DrawRoundedRect(
+		firefly.P(margin+1, margin+1),
+		size,
+		firefly.S(4, 4),
+		firefly.Solid(firefly.ColorBlack),
+	)
+	firefly.DrawRoundedRect(
+		firefly.P(margin, margin),
+		size,
+		firefly.S(4, 4),
+		firefly.Style{
+			FillColor:   firefly.ColorWhite,
+			StrokeColor: firefly.ColorBlack,
+			StrokeWidth: 1,
+		},
+	)
+}
+
+func drawLoadingBar() {
 	point := firefly.Point{X: X, Y: Y - FontHeight}
 	text := "scanning..."
 	firefly.DrawText(text, font, point, firefly.ColorLightGray)
