a824a473102ed152be5d788bd8cbf22c42b94131
gram <git@orsinium.dev>
2026-04-27 14:03:33 +0200

    show the list of connected peers

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ extern crate alloc;
 
 mod state;
 
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
 use firefly_rust::*;
 use firefly_ui::Input;
 use state::*;
@@ -18,25 +22,60 @@ extern "C" fn update() {
     let state = get_state();
     state.input.update();
     match state.scene {
-        Scene::Scanning => {
-            match state.input.get() {
-                Input::Back => quit(),
-                Input::Select => {
-                    // stop...
-                    state.cursor = 0;
-                    state.peer = 0;
-                    state.scene = Scene::List;
-                }
-                _ => {}
-            }
+        Scene::Scanning => update_scanning(state),
+        Scene::List => update_list(state),
+        Scene::PeerActions => update_peer_actions(state),
+    }
+}
+
+fn update_scanning(state: &mut State) {
+    if matches!(state.input.get(), Input::Back | Input::Select) {
+        if state.peers.len() <= 1 {
+            quit();
+            return;
         }
-        Scene::List => {}
-        Scene::PeerActions => match state.input.get() {
-            Input::Up | Input::Left => state.cursor = 0,
-            Input::Down | Input::Right => state.cursor = 1,
-            Input::Select => if state.cursor == 0 {},
-            Input::Back | Input::None => {}
-        },
+        state.cursor = 0;
+        state.peer = 0;
+        state.scene = Scene::List;
+    }
+
+    let peers = get_peers();
+    let mut names = Vec::new();
+    for peer in peers {
+        let mut name = get_name_buf(peer);
+        if name.is_empty() {
+            name = "<empty>".to_string();
+        }
+        names.push(name);
+    }
+    if names.len() > 1 && count_full_peers(&state.peers) != count_full_peers(&names) {
+        state.cursor = 0;
+        state.peer = 0;
+        state.scene = Scene::List;
+    }
+    state.peers = names;
+}
+
+fn count_full_peers(names: &[String]) -> usize {
+    let mut cnt = 0;
+    for name in names {
+        if !name.contains('?') {
+            cnt += 1;
+        }
+    }
+    cnt
+}
+
+fn update_list(state: &mut State) {
+    // ...
+}
+
+fn update_peer_actions(state: &mut State) {
+    match state.input.get() {
+        Input::Up | Input::Left => state.cursor = 0,
+        Input::Down | Input::Right => state.cursor = 1,
+        Input::Select => if state.cursor == 0 {},
+        Input::Back | Input::None => {}
     }
 }
 
@@ -59,7 +98,7 @@ fn draw_name(state: &State) {
     let Some(name) = state.peers.first() else {
         return;
     };
-    let mut point = Point::new(5, 5 + i32::from(font.char_height()));
+    let mut point = Point::new(5, i32::from(font.char_height()) - 1);
     let prefix = "hello, ";
     draw_text(prefix, &font, point, theme.primary);
     point.x += font.line_width_ascii(prefix) as i32;
@@ -70,12 +109,16 @@ fn draw_scanning(state: &State) {
     let theme = state.settings.theme;
     let font = state.font.as_font();
     let title = "scanning...";
-    let options = &["stop"];
+    let option = if state.peers.len() <= 1 {
+        "cancel"
+    } else {
+        "stop"
+    };
     firefly_ui::draw_dialog(
         theme,
         &font,
         title,
-        options,
+        &[option],
         state.cursor,
         state.input.pressed(),
     );
@@ -87,6 +130,16 @@ fn draw_list(state: &State) {
     firefly_ui::draw_bg_box(theme);
     let title = "connected peers";
     firefly_ui::draw_title(title, false, &font, theme.accent);
+
+    let line_h = font.char_height() as i32 + 4;
+    for (peer, i) in state.peers.iter().skip(1).zip(2..) {
+        let mut point = Point::new(20, 12 + i * line_h);
+        if i - 1 == i32::from(state.cursor) && state.input.pressed() {
+            point.x += 1;
+            point.y += 1;
+        }
+        draw_text(peer, &font, point, theme.primary);
+    }
 }
 
 fn draw_peer_actions(state: &State) {
