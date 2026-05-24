0d8b051a1e4a8e4be2f4a193caac1608a89ca1a3
gram <git@orsinium.dev>
2026-05-24 08:57:10 +0200

    add bg to the device name

--- src/main.rs
+++ src/main.rs
@@ -143,12 +143,12 @@ extern "C" fn render() {
     let state = get_state();
     let theme = state.settings.theme;
     firefly_ui::draw_bg_grid(theme);
-    draw_name(state);
     match state.scene {
         Scene::Scanning => draw_scanning(state),
         Scene::List => draw_list(state),
         Scene::PeerActions => draw_peer_actions(state),
     }
+    draw_name(state);
 }
 
 fn draw_name(state: &State) {
@@ -159,8 +159,23 @@ fn draw_name(state: &State) {
     let Some(name) = state.peers.first() else {
         return;
     };
-    let mut point = Point::new(5, i32::from(font.char_height()) - 1);
     let prefix = Message::Hello.translate(lang);
+
+    draw_rounded_rect(
+        Point::new(-4, -4),
+        Size::new(
+            (font.line_width_ascii(prefix) + font.line_width_ascii(name)) as i32 + 13,
+            i32::from(font.char_height()) + 10,
+        ),
+        Size::new(4, 4),
+        Style {
+            fill_color: theme.bg,
+            stroke_color: theme.primary,
+            stroke_width: 1,
+        },
+    );
+
+    let mut point = Point::new(5, i32::from(font.char_height()) - 1);
     draw_text(prefix, font, point, theme.primary);
     point.x += font.line_width_ascii(prefix) as i32;
     draw_text(name, font, point, theme.accent);
